1f94603928c6bef480770cb1778275d40a3f3f5b
gitdebraj <debraj.gupta@mindtree.com>
2019-09-21 10:22:10 +0000

    Adding changed files Dem01 and Demo2

--- app1/src/app1/Demo1.java
+++ app1/src/app1/Demo1.java
@@ -1,11 +1,14 @@
 package app1;
 import java.io.*;
 
+
 public class Demo1 {
 
 	public static void main (String args[])
 	{
 		System.out.println("First demo");
+		System.out.println("Making changes to Demo 1");
+		
 	}
 	
 }
--- app2/src/app2/Demo2.java
+++ app2/src/app2/Demo2.java
@@ -6,5 +6,6 @@ public class Demo2 {
 	public static void main (String args[])
 	{
 		System.out.println("Second demo");
+		System.out.println("Making changes to Demo 2");
 	}
 }
